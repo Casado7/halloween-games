e5b1663359b804fa463a0a5a3a6141ef9c0afe28
Alejandro Casado <alejandroacasadoc@gmail.com>
2024-10-14 23:49:12 -0400

    Refactor Die component to adjust dot positioning

--- src/games/DiceGameJs/Die.jsx
+++ src/games/DiceGameJs/Die.jsx
@@ -15,7 +15,7 @@ export default function Die({ value }) {
             case 5:
                 return [[0, 0], [0, 2], [1, 1], [2, 0], [2, 2]]; // Esquinas y centro
             case 6:
-                return [[0, 0], [0, 1], [0, 2], [2, 0], [2, 1], [2, 2]]; // Dos columnas
+                return [[0, 0], [0, 2], [1, 0], [1, 2], [2, 0], [2, 2]];  // Dos columnas
             default:
                 return [];
         }

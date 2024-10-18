d5329efb0212a153fd84e194f97de15efc0c92ee
Alejandro Casado <alejandroacasadoc@gmail.com>
2024-10-18 18:34:14 -0400

    Refactor UploadWidget component to include Cloudinary upload result handling

--- src/components/UploadWidget.jsx
+++ src/components/UploadWidget.jsx
@@ -1,14 +1,19 @@
 import { useRef, useEffect } from 'react';
-const UploadWidget = () => {
+
+const UploadWidget = ({ setUploadResult }) => {
     const cloudinaryRef = useRef();
-    const widgetRef = useRef(); 
+    const widgetRef = useRef();
     useEffect(() => {
         cloudinaryRef.current = window.cloudinary;
         widgetRef.current = cloudinaryRef.current.createUploadWidget({
             cloudName: import.meta.env.VITE_CLOUD_NAME,
             uploadPreset: import.meta.env.VITE_UPLOAD_PRESET,
-        }, function(error, result) {
+        }, function (error, result) {
             console.log(result);
+            if (result.info.files) {
+                console.log(result.info.files[0].uploadInfo.secure_url);
+                setUploadResult(result.info.files[0]);
+            }
         });
     }, []);
     return (

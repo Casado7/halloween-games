56c443c2270170b1a69c7a1006af69251b597e94
Alejandro Casado <alejandroacasadoc@gmail.com>
2024-10-20 19:04:53 -0400

    Refactor App component to add background music functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import useSound from 'use-sound';
+import backGroundMusic from './components/sounds/background.mp3';
 import DiceGame from './games/DiceGame';
 import CloudinaryTests from './components/CloudinaryTests';
 import Boss from './components/Boss';
@@ -7,8 +9,18 @@ import Boss from './components/Boss';
 function App() {
   const [startGame, setStartGame] = useState(false);
   const [playerName, setPlayerName] = useState('Jugador 1');
+  const [playBackgroundMusic, { stop: stopBackgroundMusic }] = useSound(backGroundMusic, { volume: 0.3, loop: true });
   useEffect(() => {
     console.log("startGame", startGame);
+  }
+    , [startGame]);
+
+  useEffect(() => {
+    if (startGame) {
+      playBackgroundMusic();
+    } else {
+      stopBackgroundMusic();
+    }
   }
     , [startGame]);
   return (

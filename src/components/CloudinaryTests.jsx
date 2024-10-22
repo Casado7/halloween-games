8389ba09b24e675f4f0f9b7b7dd1009edcd96ad5
Alejandro Casado <alejandroacasadoc@gmail.com>
2024-10-21 21:26:38 -0400

    Refactor CloudinaryTests component to include generative image filters for different character outfits

--- src/components/CloudinaryTests.jsx
+++ src/components/CloudinaryTests.jsx
@@ -1,59 +1,97 @@
 import { useEffect, useState } from 'react'
-import { Cloudinary } from '@cloudinary/url-gen'
 import { AdvancedImage } from '@cloudinary/react'
-
+import { generativeReplace } from "@cloudinary/url-gen/actions/effect";
 import { Resize, Effect, RoundCorners } from '@cloudinary/url-gen/actions'
-import {Actions} from '@cloudinary/url-gen'
+import { Actions } from '@cloudinary/url-gen'
 import { focusOn } from '@cloudinary/url-gen/qualifiers/gravity';
 import { face } from '@cloudinary/url-gen/qualifiers/focusOn';
 import UploadWidget from './UploadWidget'
 
 
-const cld = new Cloudinary({
-  cloud: {
-    cloudName: import.meta.env.VITE_CLOUD_NAME
-  }
-})
 
-function CloudinaryTests() {
+
+function CloudinaryTests({ cld }) {
   const [uploadResult, setUploadResult] = useState(null);
   useEffect(() => {
-  console.log("result", uploadResult)
-}, [uploadResult])
+    console.log("result", uploadResult)
+  }, [uploadResult])
+
+
   return (
     <>
-      <UploadWidget  setUploadResult={setUploadResult}/>
+      <UploadWidget setUploadResult={setUploadResult} />
 
       {uploadResult && uploadResult?.uploadInfo?.secure_url && (
         <>
           <h2>Imagen cargada:</h2>
-          {/* Imagen 1: Filtro oscuro y sombrío */}
-          <h3>Filtro oscuro y sombrío</h3>
-          <AdvancedImage 
+          {/* Imagen 1: Filtro de caballero  */}
+          <AdvancedImage
             cldImg={cld.image(uploadResult.uploadInfo.public_id)
               .resize(Resize.scale(1.5)) // Escalar la imagen
-              .effect(Effect.generativeBackgroundReplace("Cartoon Mickey House")) // Reemplazar el fondo con un color oscuro
-            } 
-            alt="Halloween Dark Filter"
+              .effect(generativeReplace().from("clothing").to("knight's armor")) // Reemplazar las gafas de sol con gafas de sol negras
+            }
+            alt="Knight's Armor"
+          />
+          {/* Imagen 2: Filtro de mago */}
+          <AdvancedImage
+            cldImg={cld.image(uploadResult.uploadInfo.public_id)
+              .resize(Resize.scale(1.5)) // Escalar la imagen
+              .effect(generativeReplace().from("clothing").to("wizard's robe with wand")) // Reemplazar las gafas de sol con gafas de sol negras
+            }
+            alt="Wizard's Robe"
+          />
+          {/* Imagen 3: Filtro de cazador */}
+          <AdvancedImage
+            cldImg={cld.image(uploadResult.uploadInfo.public_id)
+              .resize(Resize.scale(1.5)) // Escalar la imagen
+              .effect(generativeReplace().from("clothing").to("hunter's outfit with bow")) // Reemplazar las gafas de sol con gafas de sol negras
+            }
+            alt="Hunter's Outfit"
+          />
+          {/* Imagen 4: Filtro de demonio */}
+          <AdvancedImage
+            cldImg={cld.image(uploadResult.uploadInfo.public_id)
+              .resize(Resize.scale(1.5)) // Escalar la imagen
+              .effect(generativeReplace().from("clothing").to("demon's outfit with horns")) // Reemplazar las gafas de sol con gafas de sol negras
+            }
+            alt="Demon's Outfit"
+          />
+          {/* Imagen 5: Filtro de zombie */}
+          <AdvancedImage
+            cldImg={cld.image(uploadResult.uploadInfo.public_id)
+              .resize(Resize.scale(1.5)) // Escalar la imagen
+              .effect(generativeReplace().from("clothing").to("zombie's outfit with blood")) // Reemplazar las gafas de sol con gafas de sol negras
+            }
+            alt="Zombie's Outfit"
+          />
+          {/* Imagen 6: Filtro de esqueleto */}
+          <AdvancedImage
+            cldImg={cld.image(uploadResult.uploadInfo.public_id)
+              .resize(Resize.scale(1.5)) // Escalar la imagen
+              .effect(generativeReplace().from("clothing").to("skeleton's outfit with bones")) // Reemplazar las gafas de sol con gafas de sol negras
+            }
+            alt="Skeleton's Outfit"
           />
 
-          {/* Imagen 4: Enfoque en el rostro, esquinas redondeadas, formato automático */}
+          {/*Enfoque en el rostro, esquinas redondeadas, formato automático */}
           <h3>Enfoque en el rostro con esquinas redondeadas</h3>
-          <AdvancedImage 
+          <AdvancedImage
             cldImg={cld.image(uploadResult.uploadInfo.public_id)
               .resize(
                 Resize.thumbnail()  // Redimensionar a un thumbnail (miniatura)
                   .width(200)
                   .height(200)
                   .gravity(focusOn(face()))
-                  // Enfocar en el rostro
+                // Enfocar en el rostro
               )
               .roundCorners(RoundCorners.max())  // Redondear las esquinas al máximo
-         // Formato automático
-            } 
+              // Formato automático
+            }
             alt="Face Focus with Rounded Corners"
           />
-        
+          {/* Imagen 5 poner la iamgen de room de la carpeta midu-images*/}
+
+
 
         </>
       )}

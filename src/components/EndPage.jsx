d581a2e039a85aedef9631eacf46b87915faa023
Alejandro Casado <alejandroacasadoc@gmail.com>
2024-10-22 19:51:04 -0400

    Refactor EndPage component to include reset game functionality and show player choices, and update image effects based on player and boss choices

--- src/components/EndPage.jsx
+++ src/components/EndPage.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
 import './EndPage.css'; // Puedes personalizar esta hoja de estilos para darle un toque final terrorífico
+import { AdvancedImage } from '@cloudinary/react';
+import { generativeReplace } from "@cloudinary/url-gen/actions/effect";
+import { Resize } from '@cloudinary/url-gen/actions';
 
 function EndPage({ isWinner, handleResetGame, playerChoices, uploadResult, cld }) {
+    const getWinningImageEffect = () => {
+        switch (playerChoices.class) {
+            case 'Mago':
+                return generativeReplace().from("clothing").to("wizard's robe with wand");
+            case 'Caballero':
+                return generativeReplace().from("clothing").to("knight's armor");
+            case 'Cazador':
+                return generativeReplace().from("clothing").to("hunter's outfit with bow");
+            default:
+                return null;
+        }
+    };
+
+    const getLosingImageEffect = () => {
+        switch (playerChoices.bossType) {
+            case 'Demonio':
+                return generativeReplace().from("clothing").to("demon's outfit with horns");
+            case 'Zombie':
+                return generativeReplace().from("clothing").to("zombie's outfit with blood");
+            case 'Esqueleto':
+                return generativeReplace().from("clothing").to("skeleton's outfit with bones");
+            default:
+                return null;
+        }
+    };
+
     return (
         <div className="end-page">
             {isWinner ? (
@@ -16,13 +45,19 @@ function EndPage({ isWinner, handleResetGame, playerChoices, uploadResult, cld }
                 </div>
             )}
 
+
             <button onClick={handleResetGame}>
                 <i className="fas fa-redo"></i> Jugar de nuevo
             </button>
-            {/* console.log */}
-            <button onClick={ () => console.log(playerChoices, uploadResult) }>
-                Ver elecciones
-            </button>
+            {/* Mostrar la imagen dependiendo de si el jugador ganó o perdió */}
+            {uploadResult && uploadResult?.uploadInfo?.secure_url && (
+                <AdvancedImage
+                    cldImg={cld.image(uploadResult.uploadInfo.public_id)
+                        .effect(isWinner ? getWinningImageEffect() : getLosingImageEffect()) // Aplicar el efecto según el resultado
+                    }
+                    alt={isWinner ? playerChoices.class : playerChoices.bossType}
+                />
+            )}
         </div>
     );
 }

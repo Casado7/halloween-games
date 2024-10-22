f9444a5db393d5082a4950d3ced798fd8444ca90
Alejandro Casado <alejandroacasadoc@gmail.com>
2024-10-21 21:59:06 -0400

    Refactor App component to include character upload widget and start dialogue button

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,25 @@ import backGroundMusic from './components/sounds/background.mp3';
 import DiceGame from './games/DiceGame';
 import CloudinaryTests from './components/CloudinaryTests';
 import Boss from './components/Boss';
+import { Cloudinary } from '@cloudinary/url-gen'
+import UploadWidget from './components/UploadWidget';
+
+
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: import.meta.env.VITE_CLOUD_NAME
+  }
+})
 
 function App() {
   const [startGame, setStartGame] = useState(false);
+  const [startDialogue, setStartDialogue] = useState(false);
+  const [uploadResult, setUploadResult] = useState(null);
   const [playerName, setPlayerName] = useState('Jugador 1');
+  const [playerChoices, setPlayerChoices] = useState({
+    class: null, // mago, caballero, otra cosa
+    bossType: null // zombie, esqueleto, demonio
+  });
   const [playBackgroundMusic, { stop: stopBackgroundMusic }] = useSound(backGroundMusic, { volume: 0.3, loop: true });
   useEffect(() => {
     console.log("startGame", startGame);
@@ -16,26 +31,39 @@ function App() {
     , [startGame]);
 
   useEffect(() => {
-    if (startGame) {
+    if (startDialogue) {
       playBackgroundMusic();
     } else {
       stopBackgroundMusic();
     }
   }
-    , [startGame]);
+    , [startDialogue]);
   return (
     <div className="app-container">
+      {!startDialogue && !startGame && (
+        <>
+          <UploadWidget setUploadResult={setUploadResult} />
+          <button onClick={() => setStartDialogue(true)}>Empezar</button>
+        </>
+
+      )}
       {/* Si startGame es false, mostrar el componente Boss */}
-      {!startGame && (
+      {startDialogue && !startGame && (
         <div className="boss-container">
-          <Boss startGame={startGame} setStartGame={setStartGame} setPlayerName={setPlayerName} />
+          <Boss
+            startGame={startGame}
+            setStartGame={setStartGame}
+            setPlayerName={setPlayerName}
+            playerChoices={playerChoices}
+            setPlayerChoices={setPlayerChoices}
+            cld={cld} />
         </div>
       )}
 
       {/* Si startGame es true, mostrar el componente DiceGame */}
       {startGame && (
         <div className="dicegame-container">
-          <DiceGame startGame={startGame} setStartGame={setStartGame} playerName={playerName} />
+          <DiceGame startGame={startGame} setStartGame={setStartGame} playerName={playerName} cld={cld} uploadResult={uploadResult} />
         </div>
       )}
     </div>
